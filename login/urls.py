90b094f2a0fc5b9ccdecd43d5f65a9a57c5f61d0
Abhishek das <pulsepharma.dbm@gmail.com>
2024-04-11 19:13:38 +0530

    11-04-2024 login page

--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.login,name="login"),
+    path('', views.home,name="home"),
+    path('login', views.login_request,name="login_request"),
+    path('logout', views.logout_request,name="logout_request"),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-def login(request):
-    return render(request,"index.html",{})
+def home(request):
+    if request.method == 'POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'you have been logged in')
+            return redirect('home')
+        else:
+            return render(request, 'login/index.html', {'error': 'Sorry. The Username and Passwords do not match.'})
+    list=dir(request)
+    for i in list:
+        print(i)
+    
+    return render(request,"data/data.html",{'name':request.user})
+
+def login_request(request):
+    pass
+
+def logout_request(request):
+    logout(request)
+    return redirect('home')
