2e919cfa28d88d15371e4cd69cd38d674c57320c
Abhishek das <pulsepharma.dbm@gmail.com>
2024-04-16 21:16:19 +0530

    user database and login 16-04-2024

--- login/models.py
+++ login/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    FSCODE = models.CharField(max_length=20,null=True, blank=True)
+    Division = models.CharField(max_length=10, blank=True)
+    Region = models.CharField(max_length=10, blank=True)
+    Head_Quarter= models.CharField(max_length=10, blank=True)
+    Design= models.CharField(max_length=10, blank=True)
+    EMP_Code= models.CharField(max_length=10, blank=True)
+    Reporting_Manager_Name= models.CharField(max_length=100, blank=True)
+    Reporting_Manager_Code= models.CharField(max_length=10, blank=True)
+
+
+
+
+"""
+
+user_data =[{'username':'nbm1.hyderabad@pulsepharma.net','password':'8052','email':'nbm1.hyderabad@pulsepharma.net','FSCODE':'5LM156','Division':'NUCLEUS','Region':'TELANGANA','Head_Quarter':'HYDERABAD','Design':'BM','EMP_Code':'5605','Reporting_Manager_Name':'NALLURI SOLMON RAJU','Reporting_Manager_Code':'6404','first_name':'MIRZA AHMED ALI BAIG'}]
+
+for data in user_data:
+    user, created = User.objects.get_or_create(
+        username=data['username'],
+        defaults={
+            'password': data['password'],
+            'email': data['email'],
+            'first_name': data['first_name']
+        }
+    )
+    
+    if created:
+        profile = UserProfile(
+            user=user,
+            FSCODE=data['FSCODE'],
+            Division=data['Division'],
+            Region=data['Region'],
+            Head_Quarter=data['Head_Quarter'],
+            Design=data['Design'],
+            EMP_Code=data['EMP_Code'],
+            Reporting_Manager_Name=data['Reporting_Manager_Name'],
+            Reporting_Manager_Code=data['Reporting_Manager_Code']
+        )
+        profile.save()
+    else:
+        print(f"User with username {data['username']} already exists.")
+
+        """
