165af13d1cb7a11640f48783bc07c87944c0b726
Abhishek das <pulsepharma.dbm@gmail.com>
2024-04-18 10:33:31 +0530

    login information like id and passwprd with the base data 18-04-2024

--- login/models.py
+++ login/models.py
@@ -14,35 +14,16 @@ class UserProfile(models.Model):
 
 
 
-
-"""
-
-user_data =[{'username':'nbm1.hyderabad@pulsepharma.net','password':'8052','email':'nbm1.hyderabad@pulsepharma.net','FSCODE':'5LM156','Division':'NUCLEUS','Region':'TELANGANA','Head_Quarter':'HYDERABAD','Design':'BM','EMP_Code':'5605','Reporting_Manager_Name':'NALLURI SOLMON RAJU','Reporting_Manager_Code':'6404','first_name':'MIRZA AHMED ALI BAIG'}]
-
-for data in user_data:
-    user, created = User.objects.get_or_create(
-        username=data['username'],
-        defaults={
-            'password': data['password'],
-            'email': data['email'],
-            'first_name': data['first_name']
-        }
-    )
-    
-    if created:
-        profile = UserProfile(
-            user=user,
-            FSCODE=data['FSCODE'],
-            Division=data['Division'],
-            Region=data['Region'],
-            Head_Quarter=data['Head_Quarter'],
-            Design=data['Design'],
-            EMP_Code=data['EMP_Code'],
-            Reporting_Manager_Name=data['Reporting_Manager_Name'],
-            Reporting_Manager_Code=data['Reporting_Manager_Code']
+class BaseSales(models.Model):
+    Division = models.CharField(max_length=10, blank=True)
+    Region = models.CharField(max_length=10, blank=True)
+    Head_Quarter= models.CharField(max_length=10, blank=True)
+    Category = models.CharField(max_length=10, blank=True)
+    Group = models.CharField(max_length=10, blank=True)
+    Final_Base_PCPM = models.DecimalField(
+            max_digits=17,  # 10 digits before the decimal point + 5 digits after the decimal point
+            decimal_places=2,  # 5 digits after the decimal point
+                    )
+    Date=models.DateField(
+            # YYYY-MM-DD format
         )
-        profile.save()
-    else:
-        print(f"User with username {data['username']} already exists.")
-
-        """
