fd83357d629447b63319f4bfd5de9082210f17e5
Abhishek das <pulsepharma.dbm@gmail.com>
2024-04-18 11:08:41 +0530

    login information like id and passwprd with the base data 18-04-2024 settings update

--- emi_traker/settings.py
+++ emi_traker/settings.py
@@ -75,12 +75,15 @@ WSGI_APPLICATION = 'emi_traker.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pulsepharma$default',
+        'USER': 'pulsepharma',
+        'PASSWORD': 'Pulse@123',
+        'HOST': 'pulsepharma.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
-}
-
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
