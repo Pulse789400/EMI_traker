2a967d3960224f4036b7f066bda356a5c9bdbbf8
Abhishek das <pulsepharma.dbm@gmail.com>
2024-04-18 16:37:39 +0530

    login information like id and passwprd with the base data 18-04-2024 settings update7

--- emi_traker/settings.py
+++ emi_traker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nlbuq__zpsbhlofddaxozb*54!*^0f%ssjki76nqfz9c4tttz&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
